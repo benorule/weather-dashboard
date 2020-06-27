c05b9eb9b6e9aac3cb76dc518c93a4622f361eec
Benjamin Turnbull <benorule@outlook.com>
2020-06-27 10:37:10 +0930

    got data from API key

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # weather-dashboard
 Coursework Assignment No. 5 for Coding Bootcamp
+
+## Getting information from weather API
+After setting up an API key I defined a variable, queryURL, as the URL for the API. I then used an ajax function to get the information from this url and created an additional function that logs the information in the console as an object.
--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,12 @@
 // API key appid=2e8723bbb74f1b8049626523777f911b
 
-var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=London&appid=appid=2e8723bbb74f1b8049626523777f911b";
+var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=2e8723bbb74f1b8049626523777f911b"
 console.log(queryURL);
 
 $.ajax({
     url: queryURL,
     method: "GET"
+}).then(function (result) {
+
+    console.log(result);
 });
