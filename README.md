00d10d2603e75ffa1e78e4afbac364d8e1cc287f
Benjamin Turnbull <benorule@outlook.com>
2020-06-28 00:13:44 +0930

    added moment.js and bootstrap

--- README.md
+++ README.md
@@ -2,4 +2,10 @@
 Coursework Assignment No. 5 for Coding Bootcamp
 
 ## Getting information from weather API
-After setting up an API key I defined a variable, queryURL, as the URL for the API. I then used an ajax function to get the information from this url and created an additional function that logs the information in the console as an object.
+After setting up an API key I defined a variable, queryURL, as the URL for the API. I used another variable in the URL that contained the user input of city name. I then used an ajax function to get the information from this url and created an additional function that logs the information in the console as an object. I repeated this process for the second API used for the 5 day forecast with the minor change of using a for loop to display all 5. I included all of these functions within a single onclick function associated with the search button.
+
+## Bootstrap
+I implemented bootstrap into my document and firstly formatted the search bar and search button into a navbar. Next I created two cards one for each of the API's. I wrote javascript code such that the desired information from the objects from the APIs would display as desired in these cards once the search button had been pressed.
+
+## Moment.js
+I implemented moment.js into my document and retrieved the current date before adding this to the text content of one of the cards created.
--- assets/script.js
+++ assets/script.js
@@ -1,31 +1,24 @@
+console.log(moment().format("dddd, MMMM Do YYYY"));
+
+function displayFunction() {
+    var T = document.getElementById("displayDiv");
+    T.style.display = "block";  // <-- Set it to block
+}
+
 $("#searchButton").on("click", function () {
     // Get city name from user input
     var cityName = (document.getElementById("searchBar").value); // Getting value from input tag
     console.log(cityName);
 
-    // Unique API key for weather API
-    var apiKey = "2e8723bbb74f1b8049626523777f911b";
+        // Run function to display elements on page
+        displayFunction();
 
-    // Insert API key and name of city into custom URL for current weather
-    var queryURL = ("https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + apiKey);
-    console.log(queryURL);
+        // Unique API key for weather API
+        var apiKey = "2e8723bbb74f1b8049626523777f911b";
 
-    // Get data from API using URL
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function (result) {
-        console.log(result);
-
-        document.getElementById("weather").textContent = ("temperature: " + result.main.temp);
-
-    });
-
-    forecast();
-
-    function forecast() {
-        // Insert API key and name of the city into custom URL for forecast
-        var queryURL = ("https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=" + apiKey);
+        // Insert API key and name of city into custom URL for current weather
+        var queryURL = ("https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + apiKey);
+        console.log(queryURL);
 
         // Get data from API using URL
         $.ajax({
@@ -33,13 +26,43 @@ $("#searchButton").on("click", function () {
             method: "GET"
         }).then(function (result) {
             console.log(result);
+            // city name, the date, an icon representation of weather conditions, 
+            // the temperature, the humidity, the wind speed, and the UV index
 
-            document.getElementById("forecast").textContent = ("temperature: " + result.list[0].main.temp);
+            console.log(result.name);
+            console.log("temperature: " + result.main.temp);
+            document.getElementById("date").textContent = (moment().format("dddd, MMMM Do YYYY"));
+            document.getElementById("cityName").textContent = (result.name);
+            document.getElementById("weather").textContent = ("temperature: " + result.main.temp
+            + "\nhumidity: " + result.main.humidity + "\nwind speed: " + result.wind.speed);
 
         });
-    }
 
-});
+        forecast();
+
+        function forecast() {
+            // Insert API key and name of the city into custom URL for forecast
+            var queryURL = ("https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=" + apiKey);
+
+            // Get data from API using URL
+            $.ajax({
+                url: queryURL,
+                method: "GET"
+            }).then(function (result) {
+                console.log(result);
+                // 5-day forecast that displays the date, 
+                // an icon representation of weather conditions, the temperature, and the humidity
+
+                // For loop for each 5 days
+                for (i = 1; i < 6; i++) {
+                    console.log("temperature: " + result.list[i].main.temp);
+                    document.getElementById("forecast" + i).textContent = ("temperature: " + result.list[i].main.temp + "\nhumidity: " + result.list[i].main.humidity);
+                }
+
+            });
+        }
+
+    });
 
 
 
