ed0a453401f66a4cc228bcaee1b0bc0f042e15bd
Benjamin Turnbull <benorule@outlook.com>
2020-06-27 10:09:29 +0930

    linked API in javascript file

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,6 @@
 // API key appid=2e8723bbb74f1b8049626523777f911b
 
-var queryURL = "api.openweathermap.org/data/2.5/weather?q=London&appid=appid=2e8723bbb74f1b8049626523777f911b";
+var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=London&appid=appid=2e8723bbb74f1b8049626523777f911b";
 console.log(queryURL);
 
 $.ajax({
