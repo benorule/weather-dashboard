f22f47834012ac28a63383cd4f4e0a8935aec705
Benjamin Turnbull <benorule@outlook.com>
2020-06-27 17:24:38 +0930

    implemented API for the 5 day forecast

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,46 @@
-// API key appid=2e8723bbb74f1b8049626523777f911b
+$("#searchButton").on("click", function () {
+    // Get city name from user input
+    var cityName = (document.getElementById("searchBar").value); // Getting value from input tag
+    console.log(cityName);
 
-var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=2e8723bbb74f1b8049626523777f911b"
-console.log(queryURL);
+    // Unique API key for weather API
+    var apiKey = "2e8723bbb74f1b8049626523777f911b";
 
-$.ajax({
-    url: queryURL,
-    method: "GET"
-}).then(function (result) {
+    // Insert API key and name of city into custom URL for current weather
+    var queryURL = ("https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + apiKey);
+    console.log(queryURL);
 
-    console.log(result);
+    // Get data from API using URL
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (result) {
+        console.log(result);
+
+        document.getElementById("weather").textContent = ("temperature: " + result.main.temp);
+
+    });
+
+    forecast();
+
+    function forecast() {
+        // Insert API key and name of the city into custom URL for forecast
+        var queryURL = ("https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=" + apiKey);
+
+        // Get data from API using URL
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (result) {
+            console.log(result);
+
+            document.getElementById("forecast").textContent = ("temperature: " + result.list[0].main.temp);
+
+        });
+    }
+
 });
+
+
+
+
